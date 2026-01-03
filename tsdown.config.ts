12bf4308eda32e49e8e4e63736650bb520880138
alphainfinitus <kumar.mridul000@gmail.com>
2026-01-04 02:09:28 +0530

    chore: Integrate React Compiler via Babel plugin and update development dependencies.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,4 @@
+import pluginBabel from '@rollup/plugin-babel'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
@@ -9,4 +10,15 @@ export default defineConfig({
   treeshake: true,
   exports: true,
   external: ['react', 'react-dom'],
+  plugins: [
+    pluginBabel({
+      babelHelpers: 'bundled',
+      parserOpts: {
+        sourceType: 'module',
+        plugins: ['jsx', 'typescript'],
+      },
+      plugins: ['babel-plugin-react-compiler'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+  ],
 })
